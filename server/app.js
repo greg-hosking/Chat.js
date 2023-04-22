95cba2b6c80873f8921e2f3966436d8a23023e85
Greg Hosking <hoskinggregory@gmail.com>
2023-04-21 22:47:24 -0400

    Convert CommonJS code to ES

--- server/app.js
+++ server/app.js
@@ -119,9 +119,12 @@ setupRoutes(app);
 // ****************
 // Deployment Setup
 // ****************
+import { fileURLToPath } from 'url';
 import path from 'path';
 
 if (process.env.NODE_ENV === 'production') {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
   app.use(express.static(path.join(__dirname, 'build')));
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
